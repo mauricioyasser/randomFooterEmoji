9365d1911527118071fb05752cc860ed0a756fb3
Maurício Yasser <mauricio.y@outlook.com>
2022-08-05 11:47:14 -0300

    Update randomEmoji.js
    Update on array of Emojis, adding more options and brazilian memes.
    Update on 'randomEmoji' function, adding an 'if' to includes de tooltip html object.

--- assets/scripts/randomEmoji.js
+++ assets/scripts/randomEmoji.js
@@ -1,12 +1,74 @@
-// <<< Emojis Arrays >>>
-let genericEmojis = ["😂", "😎", "😊", "😍", "🥰", "🤩", "🤔", "😏", "😴", "🤑", "🤯", "🥳", "🥸", "🤓", "👽", "👾", "🤖", "🦄", "🧠", "🧑🏻‍💻", "🕺🏻", "💪🏻", "✌🏻", "🤟🏻", "🎨", "🪄", "🎵", "🎶", "🛠️", "💻", "⌨️", "🖱️", "💡", "⌚", "🍣", "🍫", "☕", "🚀", "🛸", "🏳️‍🌈", "🌌", "🌚", "🌈", "🔥", "❤️"];
+// <<< GENERIC EMOJIS >>>
+let genericEmojis = [
+    ["😊", false],
+    ["😎", false],
+    ["😍", false],
+    ["🥰", false],
+    ["🤔", "❓"],
+    ["🤨", "Meteu essa?"],
+    ["😏", "How you doin?"],
+    ["🤯", false],
+    ["🤪", false],
+    ["🥸", false],
+    ["🤓", false],
+    ["👽", "ALIENS!!"],
+    ["👾", false],
+    ["🤖", false],
+    ["🦄", false],
+    ["👁️", "👁️"],
+    ["🧠", false],
+    ["🕵🏻", false],
+    ["🥷🏻", false],
+    ["🧑🏻‍🔬", false],
+    ["👨🏻‍💻", false],
+    ["👨🏻‍🚀", false],
+    ["🕺🏻", "You should be dancing... 🎶"],
+    ["💪🏻", false],
+    ["🤏🏻", "E o salário ó..."],
+    ["✌🏻", false],
+    ["🖖🏻", "Vida longa e próspera"],
+    ["🤜🏻🤛🏻", "Tamo Junto!"],
+    ["🤟🏻", false],
+    ["🎉", false],
+    ["🎨", false],
+    ["🎯", false],
+    ["🎮", false],
+    ["🪄", "Não é feitiçaria, é tecnologia"],
+    ["🪅", false],
+    ["🎶", false],
+    ["🛠️", false],
+    ["🧪", false],
+    ["💻", false],
+    ["⌨️", false],
+    ["📸", "Diga xis!"],
+    ["💡", "Eureka!"],
+    ["🍕", false],
+    ["🍔", false],
+    ["🍣", false],
+    ["🍫", false],
+    ["☕", "Quero cafééé!"],
+    ["🚀", false],
+    ["🛸", "🐄"],
+    ["🏳️‍🌈", false],
+    ["🏖️", false],
+    ["🌈", false],
+    ["❤️", false]
+];
 
 // <<< Functions >>>
 // Executes right when the page is loaded and updates de 'innerHTML' content of the 'emojiContent' div
 window.onload = function randomEmoji() {
-    let emojiSpace = document.getElementById("emojiContent");
+    let emojiSpace = document.getElementById("footerEmoji");
 
-    emojiSpace.innerHTML = genericEmojis[generateRandom(genericEmojis)];
+    emojiArray = genericEmojis;
+    
+    let randomElement = generateRandom(emojiArray);
+
+    if(!genericEmojis[randomElement][1]){
+        emojiSpace.innerHTML = `<span id="emojiContent">${genericEmojis[randomElement][0]}</span>`
+    }else{
+        emojiSpace.innerHTML = `<span id="emojiContent">${genericEmojis[randomElement][0]}</span><span id="tooltipText">${genericEmojis[randomElement][1]}</span>`
+    }
 };
 
 // Generates a random number based on the length of the 'genericEmojis' array
@@ -14,4 +76,4 @@ function generateRandom(emojiArray) {
     const randomNumber = Math.floor(Math.random() * emojiArray.length);
 
     return randomNumber;
-};
+}
